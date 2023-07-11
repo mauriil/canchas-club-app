663cc8cda75b51dfc9863ca90435c866b734ebd7
Alaniz Gustavo <alaniz.gustavo.a@gmail.com>
2023-07-11 17:46:23 -0300

    theme implemented

---
+++ src/Themes/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#F7F0CA',
+      },
+      secondary: {
+        main: '#f19ccc',
+      },
+    },
+})
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@mui/material'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 import App from './App'
 import './index.css'
+import { theme } from "./Themes/theme";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>  
   </BrowserRouter>,
 )
