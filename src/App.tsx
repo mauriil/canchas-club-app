a29bb69eef592b2dabf78165ad5d464c839a047a
Alaniz Gustavo <alaniz.gustavo.a@gmail.com>
2023-07-12 00:46:17 -0300

    first commit for logIn style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import LogIn from './components/Login';
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { UserContext } from './contexts/userContext';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
+  const [isLogedIn, setIsLogedIn] = useState(false);
 
   return (
     <>
-
+      <UserContext.Provider value={isLogedIn}>
+        <Routes>
+          <Route path='/' element={<Navigate to='login'/>}/>
+          <Route path='/login' element={<LogIn />}/>
+        </Routes >  
+      </UserContext.Provider >
     </>
   )
 }
---
+++ src/components/Login/index.tsx
@@ -0,0 +1,133 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://canchas.club/">
+        canchas.club
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function LogIn() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        padding="2rem"
+        sx={{
+          marginTop: "0px",
+          backgroundColor: "#edebc9",
+          boxShadow: "0px 0px 25px 1px rgb(0,0,0)",
+          borderRadius: "15px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.dark" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            sx={{ background: "transparent" }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Password"
+            name="password"
+            autoComplete="current-password"
+            type="password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            color="primary"
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 0, mb: 1 }}
+          >
+            LogIn
+          </Button>
+          <Button
+            color="primary"
+            type="button"
+            fullWidth
+            variant="outlined"
+            sx={{ mt: 1, mb: 0 }}
+          >
+            Sign In
+          </Button>
+          <Grid container sx={{ mt: 1, mb: 0 }}>
+            <Grid item xs>
+              <Link href="#" variant="body1">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body1">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+        <Copyright sx={{ mt: 4, mb: 3 }} />
+      </Box>
+    </Container>
+  );
+}
