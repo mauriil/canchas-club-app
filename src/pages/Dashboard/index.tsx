2a40fac6e370f2accecd3bca80baca36d8f345f6
Mauri Lahitte <mauriciolahitte@Mauricios-MacBook-Air.local>
2023-07-27 17:57:15 -0300

    adding bottom navigation

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
-import { Box } from "@mui/material";
 import { useAuth } from "../../customHooks/useAuth";
 import Title from "../../components/Title";
 import logo from "../../assets/images/canchasCubLogo.png";
+import BottomNavigation from "../../components/BottomNavigation";
+import { Box } from "@mui/material";
 
 const Dashboard = () => {
   const { user } = useAuth();
@@ -12,13 +13,14 @@ const Dashboard = () => {
         flexDirection: { md: "row", xs: "column" },
       }}
       display="flex"
-      justifyContent="space-around"
+      justifyContent="space-between"
       bgcolor={"primary.dark"}
       alignItems="center"
       height="100vh"
       width="100vw"
     >
       <Title firtLineTitle={user ? user.userName : ""} />
+      <BottomNavigation />
     </Box>
   );
 };
