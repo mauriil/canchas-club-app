4cf9f489515785f664a658ba0eb7b0982f07cbb1
Mauri Lahitte <mauriciolahitte@Mauricios-MacBook-Air.local>
2023-11-14 16:05:49 -0300

    improve MisReservas page

--- src/pages/MisReservas/index.tsx
+++ src/pages/MisReservas/index.tsx
@@ -1,74 +1,63 @@
+import React, { useEffect, useState } from "react";
 import { Box, Grid, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { getAllBookingsByOwner, getAllBookingsByUser } from "../../api/bookings";
+import { getAllBookingsByUser } from "../../api/bookings";
 import BookingCard from "../../components/BookingCard";
 import { Booking } from "../../types/booking";
 import CanchasClubLoader from "../../components/Loader";
 
 const MisReservas = () => {
-  const [booking, setBooking] = useState<Booking[]>([]);
+  const [bookings, setBookings] = useState<Booking[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const getBookings = async () => {
-    setIsLoading(true);
-    const req = await getAllBookingsByUser();
-    const bookingsOfUser: Booking[] = (await req.json()) as Array<Booking>;
-
-    setBooking(bookingsOfUser);
-    setIsLoading(false);
+  const fetchBookings = async () => {
+    try {
+      setIsLoading(true);
+      const response = await getAllBookingsByUser();
+      const data: Booking[] = await response.json();
+      setBookings(data[0]);
+    } catch (error) {
+      console.error("Error fetching bookings:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   useEffect(() => {
-    void getBookings();
+    void fetchBookings();
   }, []);
 
   return (
     <Box
-      width="100vw"
-      height="100vh"
+      width="100%"
+      minHeight="100vh"
       display="flex"
       justifyContent="center"
       alignItems="center"
+      p={3}
     >
-      {isLoading ? <CanchasClubLoader width="80%" /> :
-
-        booking.length > 0 ? (
-          <Grid
-            container
-            rowSpacing={2}
-            columnSpacing={{ xs: 0, sm: 2, md: 3 }}
-            sx={{
-              flexGrow: 1,
-              paddingLeft: 0,
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            {booking.map((booking, index) => {
-              return (
-                <Grid
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  key={index}
-                  item
-                  xs={12}
-                  sm={6}
-                  lg={4}
-                  xl={3}
-                >
-                  <BookingCard booking={booking} />
-                </Grid>
-              );
-            })}
-          </Grid>
-        ) : (
-          <Typography variant="h3" color="primary">
-            No tenés ninguna reserva de canchas realizada
-          </Typography>
-        )
-
-      }
-
+      {isLoading ? (
+        <CanchasClubLoader width="80%" />
+      ) : bookings.length > 0 ? (
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+        >
+          {bookings.map((booking, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4} lg={3} xl={3}>
+              <BookingCard booking={booking} />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="h1" color="primary" sx={{
+          textAlign: "center",
+          fontSize: "3.5rem",
+          fontWeight: "bold",
+        }}>
+          No tienes ninguna reserva de canchas realizada
+        </Typography>
+      )}
     </Box>
   );
 };
