47ae178e1285cf3c2cd22d84058a759d9ad074d0
Mauricio Lahitte <mauriciolahitte@MAC51.local>
2023-12-15 15:17:51 -0300

    calendar view in misReservas

--- src/pages/MisReservas/index.tsx
+++ src/pages/MisReservas/index.tsx
@@ -1,19 +1,41 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import React, { useEffect, useState } from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { getAllBookingsByUser } from "../../api/bookings";
-import BookingCard from "../../components/BookingCard";
-import { Booking } from "../../types/booking";
+import { Booking, BookingDetail } from "../../types/booking";
 import CanchasClubLoader from "../../components/Loader";
+import { Calendar, dateFnsLocalizer,  } from "react-big-calendar";
+import moment from "moment";
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import { es } from "date-fns/locale";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+
+const locales = {
+  "es-ES": es
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+});
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 
 const MisReservas = () => {
-  const [bookings, setBookings] = useState<Booking[]>([]);
+  const [bookings, setBookings] = useState<BookingDetail[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchBookings = async () => {
     try {
       setIsLoading(true);
       const response = await getAllBookingsByUser();
-      const data: Booking[] = await response.json();
+      const data: BookingDetail[] = await response.json();
       setBookings(data);
     } catch (error) {
       console.error("Error fetching bookings:", error);
@@ -29,26 +51,96 @@ const MisReservas = () => {
   return (
     <Box
       width="100%"
-      minHeight="100vh"
+      height="80%"
       display="flex"
       justifyContent="center"
-      alignItems="center"
-      p={3}
+      paddingLeft={1}
+      paddingRight={1}
+      paddingTop={1}
+      marginTop={3}
+      marginBottom={3}
     >
       {isLoading ? (
         <CanchasClubLoader width="80%" />
       ) : bookings.length > 0 ? (
-        <Grid
-          container
-          spacing={2}
-          justifyContent="center"
-        >
-          {bookings.map((booking, index) => (
-            <Grid item key={index} xs={12} sm={6} md={4} lg={3} xl={3}>
-              <BookingCard booking={booking} />
-            </Grid>
-          ))}
-        </Grid>
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={
+            bookings.map((booking, index) => ({
+              start: moment(`${booking.time.day}T${booking.time.from}`).toDate(),
+              end: moment(`${booking.time.day}T${booking.time.to}`).toDate(),
+              title: booking.fieldId.name,
+            }))
+          }
+          messages={{
+            next: ">",
+            previous: "<",
+            today: "Hoy",
+            month: "Mes",
+            week: "Semana",
+            day: "Día",
+            agenda: "Agenda",
+            date: "Fecha",
+            time: "Hora",
+            event: "Evento",
+            noEventsInRange: "No hay eventos en este rango",
+            showMore: (total) => `+ Ver más (${total})`,
+            allDay: "Todo el día",
+            tomorrow: "Mañana",
+            work_week: "Semana laboral",
+            yesterday: "Ayer",
+          }}
+          culture={"es-ES"}
+          style={{
+            height: "80vh",
+            maxHeight: "700px",
+            width: "100%",
+            backgroundColor: "white",
+            padding: "1rem",
+            borderRadius: "1rem",
+            boxShadow: "0 0 10px rgba(0,0,0,0.2)",
+            gap: "1rem",
+            fontSize: "1rem",
+          }}
+          formats={{
+            monthHeaderFormat: (date, culture, localizer) => localizer.format(date, "MMMM", culture).charAt(0).toUpperCase() + localizer.format(date, "MMMM Y", culture).slice(1),
+          }}
+          eventPropGetter={(event) => ({
+            style: {
+              backgroundColor: "transparent",
+              color: "black",
+            },
+          })}
+          views={["month", "day", 'agenda']}
+          components={{
+            month: {
+              event: ({ event, ...props }) => (
+                  <Button variant="contained" color="primary" sx={{
+                    width: "100%",
+                    height: "10px",
+                    textTransform: "none",
+                    fontSize: "1rem",
+                  }}>
+                    {event.title}
+                  </Button>
+              ),
+            },
+            day: {
+              event: ({ event, ...props }) => (
+                  <Button variant="contained" color="primary" sx={{
+                    width: "100%",
+                    height: "10px",
+                    textTransform: "none",
+                    fontSize: "1rem",
+                  }}>
+                    {event.title}
+                  </Button>
+              ),
+            },
+          }}
+        />
       ) : (
         <Typography variant="h1" color="primary" sx={{
           textAlign: "center",
--- src/types/booking/index.ts
+++ src/types/booking/index.ts
@@ -9,4 +9,71 @@ export interface Booking {
     time: {from: string, to: string, day: string};
     updatedAt?: string;
     _id?: string | undefined;
-  }
+}
+
+export interface BookingDetail {
+  ownerId: string;
+  createdAt?: string;
+  paymentId: string;
+  fieldId: [{
+    availability: [{
+      monday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+      tuesday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+      wednesday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+      thursday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+      friday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+      saturday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+      sunday: [{
+        openHour: string;
+        closeHour: string;
+        price: number;
+      }],
+    }],
+    _id: string;
+    name: string;
+    description: string;
+    photos: string[];
+    sport: string;
+    fieldType: string;
+    floorType: string;
+    illumination: boolean;
+    clubId: {
+      _id: string;
+      name: string;
+      address: string;
+      userId: string;
+    }
+    createdAt: string;
+    updatedAt: string;
+  }];
+  recurrent: boolean;
+  status?: string;
+  tenantId: string;
+  time: {from: string, to: string, day: string};
+  updatedAt?: string;
+  _id?: string | undefined;
+}
